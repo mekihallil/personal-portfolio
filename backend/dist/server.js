2a714089d6bdffd7d380cf122bf1c94558c95f55
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-03-27 16:30:25 +0300

    initializing environment

---
+++ backend/dist/server.js
@@ -0,0 +1,6 @@
+import express from "express";
+const server = express();
+const PORT = 5000;
+server.listen(PORT, () => {
+    console.log("Server is running");
+});
---
+++ backend/src/server.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+const server = express();
+const PORT = 5000;
+
+server.listen(PORT, () => {
+  console.log("Server is running");
+});
