86747a33b4e2940521504cfa908d14924cd7e5d8
Meki Hallil <107886787+mekihallil@users.noreply.github.com>
2026-03-28 07:09:38 +0300

    Menubar added

--- frontend/pages/Dashboard.tsx
+++ frontend/pages/Dashboard.tsx
@@ -4,7 +4,6 @@ import {
   MenubarGroup,
   MenubarItem,
   MenubarMenu,
-  MenubarSeparator,
   MenubarTrigger,
 } from "@/components/ui/menubar";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
@@ -12,24 +11,19 @@ import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import type { FC, ReactElement } from "react";
 export const Dashboard: FC = (): ReactElement => {
   return (
-    <Menubar className="display-flex justify-end border-none ">
+    <Menubar className=" justify-end border-none ">
       <MenubarMenu>
-        <MenubarTrigger className="mt-12 mr-12 ">
+        <MenubarTrigger className=" mt-12 mr-12 ">
           <MenuRoundedIcon fontSize="large" />
         </MenubarTrigger>
-        <MenubarContent className="dispaly-flex justify-items-center border-none text-3xl min-w-screen min-h-screen">
-          <MenubarGroup>
-            <MenubarItem>Home</MenubarItem>
-            <MenubarItem>About Me</MenubarItem>
-            <MenubarItem>Experiance</MenubarItem>
-            <MenubarItem>Project</MenubarItem>
-            <MenubarItem>Contact</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem>Share</MenubarItem>
-            <MenubarItem>Download CV</MenubarItem>
+        <MenubarContent className="border-none text-3xl min-w-screen min-h-screen">
+          <MenubarGroup className="justify-items-center text-center mt-20 ">
+            <MenubarItem className="text-6xl">Home</MenubarItem>
+            <MenubarItem className="text-6xl">About Me</MenubarItem>
+            <MenubarItem className="text-6xl">Experiance</MenubarItem>
+            <MenubarItem className="text-6xl">Project</MenubarItem>
+            <MenubarItem className="text-6xl">Contact</MenubarItem>
           </MenubarGroup>
-
-          <MenubarGroup></MenubarGroup>
         </MenubarContent>
       </MenubarMenu>
     </Menubar>
